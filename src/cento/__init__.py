4df1b93ad6d1d4693d9c480adb0cdda352f2669f
WithPrecedent <coreyrayburnyung@gmail.com>
2024-04-08 22:53:16 -0500

    Initial commit

---
+++ src/cento/__init__.py
@@ -0,0 +1,10 @@
+"""Combines markdown files into publications and related materials"""
+
+from __future__ import annotations
+
+
+__version__ = '0.1.0'
+
+__author__: str = 'Corey Rayburn Yung'
+
+__all__: list[str] = []
